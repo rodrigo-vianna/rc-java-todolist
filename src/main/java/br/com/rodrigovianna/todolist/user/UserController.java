efb7d075207c3e96d0727a446aef72239432b50d
Rodrigo Pillar Vianna <rpvianna2009@gmail.com>
2023-10-10 16:32:37 -0300

    Finished second lesson

--- src/main/java/br/com/rodrigovianna/todolist/user/UserController.java
+++ src/main/java/br/com/rodrigovianna/todolist/user/UserController.java
@@ -1,5 +1,11 @@
 package br.com.rodrigovianna.todolist.user;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,9 +15,23 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/users")
 public class UserController {
 
+  @Autowired
+  private IUserRepository userRepository;
+
   @PostMapping("/")
-  public void create(@RequestBody UserModel userModel) {
-    System.out.println(userModel);
+  public ResponseEntity create(@RequestBody UserModel userModel) {
+    final var user = this.userRepository.findByUsername(userModel.getUsername());
+    if (user != null) {
+      return ResponseEntity.status(HttpStatus.CONFLICT).body("User already exists");
+    }
+    final var userCreated = this.userRepository.save(userModel);
+    return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
+  }
+
+  @GetMapping("/")
+  public List<UserModel> list() {
+    final var list = this.userRepository.findAll();
+    return list;
   }
 
 }
